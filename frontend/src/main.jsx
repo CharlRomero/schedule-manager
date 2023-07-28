9af27359007e72d77abd6b1f37938f657e5d9729
CharlRomero <caromero17@espe.edu.ec>
2023-07-27 20:57:43 -0500

    frontend folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import { Schedule } from "./pages/Schedule.jsx";
 
+import "./sass/style.scss";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
