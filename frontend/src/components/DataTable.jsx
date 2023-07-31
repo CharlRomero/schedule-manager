93c7b60094723a4c68b6bd6895963c7adbc5c95e
CharlRomero <caromero17@espe.edu.ec>
2023-07-31 00:38:24 -0500

    added style button

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -2,7 +2,7 @@ export const DataTable = ({ title, thead, children, className }) => {
   return (
     <section className={className}>
       <div className="DataTable-title">
-        <h2>{title}</h2>
+        <h2 className="DataTable-title--h2">{title}</h2>
       </div>
 
       <table className="DataTable-table">
--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -62,21 +62,27 @@ export const Room = () => {
 
   return (
     <section className="Table">
-      <Button className="Button" title="Agregar" onClick={toggleCreate}>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="Button-svg"
+      <section className="Table-buttons">
+        <Button
+          className="Button Table-buttons--right"
+          title="Agregar"
+          onClick={toggleCreate}
         >
-          <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z" />
-        </svg>
-      </Button>
+          <svg
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            className="Button-svg"
+          >
+            <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z" />
+          </svg>
+        </Button>
+      </section>
       <DataTable
         className="DataTable DataTable--resize"
-        title="Paralelos"
+        title="Tabla de Paralelos"
         thead={thead}
       >
         {rooms.map((item, key) => (
