441bc26c955244259ff545086d18d58d406935ea
CharlRomero <romeokarlos12@gmail.com>
2023-08-12 20:31:09 -0500

    Added delete endpoint slot

--- server/routes/slot.routes.js
+++ server/routes/slot.routes.js
@@ -4,6 +4,7 @@ import {
   getSlot,
   createSlot,
   updateSlot,
+  deleteSlot,
 } from "../controllers/slot.controller.js";
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.get("/slot", getSlot);
 router.post("/slot", createSlot);
 router.patch("/slot/:id", updateSlot);
+router.delete("/slot/:id", deleteSlot);
 
 export default router;
